3fffbbede79155b8d7112088ff44e1be0f2da7be
mmaaxxii <maximilianoespeche@gmail.com>
2023-04-26 00:55:44 -0300

    vercel publish 6.0

--- src/pages/Portfolio/components/about-me/componets/AboutMeSocialButtons/AboutMeSocialButtons.tsx
+++ src/pages/Portfolio/components/about-me/componets/AboutMeSocialButtons/AboutMeSocialButtons.tsx
@@ -11,7 +11,7 @@ const AboutMeSocialButtons : React.FC<AboutMeSocialButtonsProps> = () => {
 			<li><a href="https://www.linkedin.com/in/maximiliano-espeche-50442314a/" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i></a></li>
 			<li><a href="https://www.facebook.com/maxiespeche/" target="_blank" rel="noreferrer"><i className="fa fa-facebook"></i></a></li>
 			<li><a href="https://www.instagram.com/max_espeche/" target="_blank" rel="noreferrer"><i className="fa fa-instagram"></i></a></li>
-			<li><a href="https://www.github.com/mmaaxxii" target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a></li>
+			<li><a href="https://github.com/mmaaxxii/portfolio" target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a></li>
 		</ul>
 	</AboutMeSocialButtonsDiv>
 	);
